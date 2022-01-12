dcaf329aa383d626a927872e095623eb370a9de9
sandylee <sandylee@noodoe.com>
2022-01-12 13:47:23 +0800

    add solutions to TDD remover from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let result = array;
+    let copyToRemove = toRemove;
+    const toRemoveLength = toRemove.length;
+    for (i = 0; i < toRemoveLength; i++)
+    {
+        let nextRemove = copyToRemove.shift();
+        
+        result = result.filter(content => content !== nextRemove);
+    }
+    return result;
 };
 
 // Do not edit below this line
