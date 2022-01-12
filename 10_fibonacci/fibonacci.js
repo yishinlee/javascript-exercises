192a0a033da16247d02696e90ab4f3adeb4224b4
sandylee <sandylee@noodoe.com>
2022-01-12 17:47:43 +0800

    add solutions to fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index == 1 || index == 2)
+        return 1;
+    else if (index < 0)
+        return "OOPS";
+    else {
+        let currentFib = 1;
+        let lastFib = 1;
+        let temp = currentFib;
+        for (let i = 3; i <=index; i++) {
+            temp = currentFib + lastFib;
+            lastFib = currentFib;
+            currentFib = temp;
+        }
+        return currentFib;
+    }
 };
 
 // Do not edit below this line
