8d53643135e40c1757b1e0e7333a00a8db3a14b1
sandylee <sandylee@noodoe.com>
2022-01-12 17:37:14 +0800

    add solution to palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let removePunctAndSpace = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    let toArray = Array.from(removePunctAndSpace.toLowerCase());
+    
+    for (let i = 0; i < Math.trunc(toArray.length / 2); i++) {
+        if (toArray[i] !== toArray[toArray.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
