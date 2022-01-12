afc74b274f8954e5aa48f1acc0d76c5b99a810d8
sandylee <sandylee@noodoe.com>
2022-01-12 15:10:38 +0800

    add solutions to TDD exercise temperature conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const c = (fahrenheit - 32) * 5 / 9;
+  return parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  const f = (celcius * 9 / 5) + 32;
+  return parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line
