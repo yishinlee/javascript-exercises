2f38ae0a19075fa5ccdf50280232d7eb3abb0f15
sandylee <sandylee@noodoe.com>
2022-01-12 13:55:43 +0800

    add solutions to TDD sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(range1, range2) {
+    let start;
+    let end;
+    let sum = 0;
+    if (typeof(range1) !== "number" || typeof(range2) !== "number")
+        return "ERROR";
+    else if (range1 < 0 || end < 0)
+        return "ERROR";
+        
+    if (range1 < range2)
+    {
+        start = range1;
+        end = range2;
+    } else {
+        start = range2;
+        end = range1;
+    }
+    for (let i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
