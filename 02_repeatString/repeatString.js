00543c15d55da79a2ef4b6bc0784d640a2367220
sandylee <sandylee@noodoe.com>
2022-01-12 11:33:53 +0800

    add solutions for TDD repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let finalString = "";
+    if (times < 0)
+        return "ERROR";
+    for (let i = 0; i < times; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
