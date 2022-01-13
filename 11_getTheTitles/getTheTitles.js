d8f7be14b04a8177f58a6f582cad42f77b8d3c8c
sandylee <sandylee@noodoe.com>
2022-01-13 10:57:29 +0800

    add solution to get the titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    let titles = [];
+    for (let i = 0; i < bookList.length; i++) {
+        titles.push(bookList[i].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
