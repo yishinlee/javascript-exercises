5946f625a001a8a0ba9b7b18c84b2452a2ac339f
sandylee <sandylee@noodoe.com>
2022-01-13 11:07:53 +0800

    add solution to find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleList) {
+    let oldest;
+    let oldestAge = 0;
+    
+    for (let i = 0; i < peopleList.length; i++) {
+        if("yearOfDeath" in peopleList[i])
+            curPersonAge = peopleList[i].yearOfDeath - peopleList[i].yearOfBirth;
+        else
+            curPersonAge = new Date().getFullYear() - peopleList[i].yearOfBirth;
+        
+        if (curPersonAge > oldestAge) {
+            oldestAge = curPersonAge;
+            oldest = peopleList[i];
+        }
+    }
+    
+    return oldest;
 };
 
 // Do not edit below this line
